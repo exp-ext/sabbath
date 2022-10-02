e222c858d363092976a1ca0f46dd622bfd821b02
exp-ext <ext77@yandex.ru>
2022-10-02 17:31:21 +0300

    add

--- yatube/users/admin.py
+++ yatube/users/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'phone_number',
+        'location',
+        'birth_date',
+        'is_staff'
+    )
+    search_fields = ('username',)
+    list_filter = ('groups',)
+    empty_value_display = '-пусто-'
